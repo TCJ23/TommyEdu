caf5bef30662f68f1438189841e8ac97c57d5bac
TCJ23 <tcj2012@gmail.com>
2018-06-30 13:58:20 +0200

    Issue#9 palindrome found error on baeldung site - if decrementingg and for i where i begins at index it must me string length -1

---
+++ src/main/java/strings_Issue_9/PalindromeStream.java
@@ -0,0 +1,30 @@
+package strings_Issue_9;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+//https://www.hackerrank.com/challenges/java-string-reverse/problem
+//http://www.baeldung.com/java-palindrome
+public class PalindromeStream {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+
+        if (A.length() < 50) {
+            if (isPalindromeUsingIntStream(A)) {
+                System.out.println("Yes");
+            }
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isPalindromeUsingIntStream(String text) {
+        String temp = text.replaceAll("\\s+", "").toLowerCase();
+        return IntStream.range(0, temp.length() / 2)
+                .noneMatch(i -> temp.charAt(i) != temp.charAt(temp.length() - i - 1));
+    }
+
+}
