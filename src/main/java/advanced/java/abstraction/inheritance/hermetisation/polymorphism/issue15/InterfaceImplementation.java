c6dd329eca7fbc6cb2860c30daccc3fcdad48be4
TCJ23 <tcj2012@gmail.com>
2018-07-18 20:24:26 +0200

    #Issue 15 abstract class

---
+++ src/main/java/advanced/java/abstraction/inheritance/hermetisation/polymorphism/issue15/InterfaceImplementation.java
@@ -0,0 +1,47 @@
+package advanced.java.abstraction.inheritance.hermetisation.polymorphism.issue15;
+
+import java.time.LocalDateTime;
+
+interface InterfaceImplementation {
+    abstract int calculateMe();
+    Integer casting();
+
+    static void calleMeStaticallyWithoutInstantiation(LocalDateTime data, int trwanie) {
+
+        System.out.println("DON'T INSTANTIATE ME");
+    }
+}
+
+class Implementacja implements InterfaceImplementation {
+    /*@Override
+     int calculateMe() {
+        return 0;
+    }*/
+
+    @Override
+    public int calculateMe() {
+        System.out.println("I must have public modifier - not WEAKER ACCESS " + 100);
+        return 100;
+    }
+
+    @Override
+    public Integer casting() {
+        return Integer.parseInt("100");
+    }
+
+    static String calleMeStaticallyWithoutInstantiation(){
+        String s = "I AM NOT SAME STATIC METHOD THAT YOU THINK";
+        System.out.println(s.toString());
+        return s;
+
+    }
+
+    public static void main(String[] args) {
+        InterfaceImplementation.calleMeStaticallyWithoutInstantiation(LocalDateTime.now(), 100);
+        System.out.println("Dont do : Implementacja imp = new InterfaceImplementaion");
+        Implementacja implementacja = new Implementacja();
+        calleMeStaticallyWithoutInstantiation();
+        implementacja.calculateMe();
+        implementacja.casting();
+    }
+}
