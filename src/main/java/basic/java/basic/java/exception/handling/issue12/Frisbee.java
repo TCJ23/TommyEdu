276c3f645aae557eac930f23a5da62f7b83bf158
TCJ23 <tcj2012@gmail.com>
2018-07-22 22:41:59 +0200

    #Issue12 p 342 pytanie do Adama Exercise 6-3 czy dobrze?

---
+++ src/main/java/basic/java/basic/java/exception/handling/issue12/Frisbee.java
@@ -0,0 +1,12 @@
+package basic.java.basic.java.exception.handling.issue12;
+
+import java.io.IOException;
+
+public class Frisbee {
+    public static void main(String[] args) throws RuntimeException {
+
+        int x = 0;
+        System.out.println(7 / x);
+    }
+
+}
---
+++ src/main/java/basic/java/basic/java/exception/handling/issue12/Propagate.java
@@ -0,0 +1,27 @@
+package basic.java.basic.java.exception.handling.issue12;
+
+public class Propagate {
+    static String s;
+    public static void main(String[] args) {
+        try {
+            Propagate.reverse(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("Main has finished its life");
+        }
+    }
+
+    private static String reverse(String s) throws Exception {
+        if (s.length() == 0) {
+            throw new Exception();
+        } else {
+            String reverseStr = "";
+            for (int i = s.length() - 1; i >= 0; --i) {
+                reverseStr += s.charAt(i);
+            }
+            return reverseStr;
+        }
+    }
+}
