5b465673b3649d35993a59f62dc3c98b561b9a3f
TCJ23 <tcj2012@gmail.com>
2018-07-30 15:35:41 +0200

    To String corrected example

--- src/main/java/advanced/java/toString/issue19/ToString.java
+++ src/main/java/advanced/java/toString/issue19/ToString.java
@@ -1,6 +1,6 @@
 package advanced.java.toString.issue19;
 
-public class ToString {
+class ToString {
     private String name = "Tomasz";
     private String lastname = "Jurek";
     private String email = "t.k.jurek@gmail.com";
@@ -11,11 +11,22 @@ public class ToString {
         return name + " " + lastname + " No EMAIL " + nickname + " AND REMEMBER ALWAYS KISS - \n KEEP IT SIMPLE STUPID";
     }
 
+
     public static void main(String[] args) {
         ToString toString = new ToString();
-        /*System.out.println("That's just my hash " + toString + "\n which would be exact as default toString method");
-        System.out.println(" return getClass().getName() + \"@\" + Integer.toHexString(hashCode()); ---> MAKES ----> " + toString);*/
+        NormalString normalString = new NormalString();
+
+        System.out.println("That's just my hash " + normalString + "\n which would be exact as default toString method");
+        System.out.println(" return getClass().getName() + \"@\" + Integer.toHexString(hashCode()); ---> MAKES ----> " + normalString);
         System.out.println("So if I override default Object toString method to let say hide my email, this would be something like this ");
         System.out.println(toString.toString());
     }
+
+}
+
+class NormalString {
+    private String name = "Tomasz";
+    private String lastname = "Jurek";
+    private String email = "t.k.jurek@gmail.com";
+    public String nickname = "Tommy";
 }
