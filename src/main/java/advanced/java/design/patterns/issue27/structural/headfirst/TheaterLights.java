59b46eb517a4773343f7655354704e393ab6c0c2
TCJ23 <tcj2012@gmail.com>
2018-09-13 14:30:14 +0200

    Facade Headfirst hometheather

---
+++ src/main/java/advanced/java/design/patterns/issue27/structural/headfirst/TheaterLights.java
@@ -0,0 +1,25 @@
+package advanced.java.design.patterns.issue27.structural.headfirst;
+
+public class TheaterLights {
+	String description;
+
+	public TheaterLights(String description) {
+		this.description = description;
+	}
+
+	public void on() {
+		System.out.println(description + " on");
+	}
+
+	public void off() {
+		System.out.println(description + " off");
+	}
+
+	public void dim(int level) {
+		System.out.println(description + " dimming to " + level  + "%");
+	}
+
+	public String toString() {
+		return description;
+	}
+}
